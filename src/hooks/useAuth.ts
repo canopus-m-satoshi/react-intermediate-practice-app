8164d81828125a3be70d00434e97076ccf0ba340
Satoshi Matsushima <>
2021-07-15 01:53:54 +0900

    added login message using Chakra Ui

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import { useCallback, useState } from 'react';
 import axios from 'axios';
 import { User } from '../types/api/user';
 import { useHistory } from 'react-router-dom';
+import { useMessage } from './useMessage';
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -16,17 +18,18 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: 'A-OK', status: 'success' });
             history.push('/home');
           } else {
-            alert('ユーザーが見つかりません');
+            showMessage({ title: 'You are not a member', status: 'error' });
           }
         })
-        .catch(() => alert('ログインできません'))
+        .catch(() => showMessage({ title: 'You fucked up', status: 'error' }))
         .finally(() => {
           setLoading(true);
         });
     },
-    [history],
+    [history, showMessage],
   );
   return { login, loading };
 };
