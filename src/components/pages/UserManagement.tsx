0af2be6c7e2dcf99eb8df250971ad1c970da70fc
Satoshi Matsushima <>
2021-07-16 00:04:37 +0900

    create custom hook for fetching users data

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,38 @@
-import { memo, VFC } from 'react';
-import { Wrap, WrapItem } from '@chakra-ui/react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, VFC } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
 import { UserCard } from '../organisms/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="test"
-          fullName="full name"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner size="xl" color="red.500" />
+        </Center>
+      ) : (
+        <Wrap
+          spacing={{ base: '15px', md: '40px' }}
+          p={{ base: 4, md: 10 }}
+          justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
