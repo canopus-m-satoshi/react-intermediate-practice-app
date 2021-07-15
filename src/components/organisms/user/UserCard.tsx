295c12bbf1a321a12869bb7c71dcd380354bb69d
Satoshi Matsushima <>
2021-07-15 23:12:43 +0900

    created UserCard component

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,39 @@
+import { memo, VFC } from 'react';
+import { Box, Image, Stack, Text } from '@chakra-ui/react';
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      p={4}
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      _hover={{ cursor: 'pointer', opacity: 0.8 }}>
+      <Stack textAlign="center">
+        <Image
+          src={imageUrl}
+          alt={imageUrl}
+          m="auto"
+          boxSize="160"
+          borderRadius="full"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" fontWeight="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
